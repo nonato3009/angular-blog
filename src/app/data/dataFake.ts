13deefa036f6bb19931eaf0bd3210d7aebdefe66
nonato3009 <nonato3@hotmail.com>
2023-10-01 18:19:30 -0300

    Background-color altered and images in the dataFake

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
   {
     "id": "1",
-    "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/80C64C0B63382CD3ED2653356125F275F63D036028A77D38DC3286AD851A6833/scale?width=1200&aspectRatio=1.78&format=jpeg",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQC6xTwdqgz3yQdICaPpdBj-GqPlc3aLd9ojw&usqp=CAU",
     "title": "NOVO HOMEM DE FERRO 5 ANUNCIADO!",
     "description": "Marvel Studios anuncia novo filme do homem de ferro!",
   },
   {
     "id": "2",
-    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg",
+    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2022/08/Marvel-Studios-MCU-750x422.jpg",
     "title": "Nova Série anunciada no Disney Plus +",
     "description": "Este filme não vai sair da sua cabeça",
+  },
+  {
+    "id": "3",
+    "photoCover": "https://mundonegro.inf.br/wp-content/uploads/2021/06/cropped-Pantera-Negra-1-1024x576.jpg",
+    "title": "Novo filme da Pantera Negra é top",
+    "description": "Wakanda pra sempre, de um jeito como você nunca viu",
+  },
+  {
+    "id": "4",
+    "photoCover": "https://www.cinemaemcena.com.br/uploads/2_destaque_vestindoofilme_xmen_capa.jpg",
+    "title": "X-Men Evolution",
+    "description": "A evolução continua, onde o poderes aumentam e as responsabilidades també",
   }
 ]
